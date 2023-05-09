ab4d6eb224aa86371f9e236a69fce888ef58a64a
kjh073 <9279482@naver.com>
2023-05-10 05:07:44 +0900

    solve sending request forever

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const mysql = require('mysql')
 const dbconfig = require('./config/database.js')
 const s3config = require('./config/s3.js')
 const bodyParser = require('body-parser')
-// const router = express.Router();
 const upload = require('./modules/multer.js');
+// const uploadRouter = require('./routes/uploadRouter');
 // const { controller } = require('./controllers');
 const connection = mysql.createConnection(dbconfig)
 app.use(bodyParser.urlencoded({ extended: true }))
 
-connection.connect(function(err) {
+
+connection.connect(err => {
 	if (err) throw err;
 	console.log('db connected')
 });
@@ -25,21 +26,50 @@ app.get('/select', (req, res) => {
 })
 
 //db에 링크랑 아이디 저장해야돼서 /upload/:id로 아이디를 받아야 할듯?
+// app.use('/', uploadRouter);
 app.post('/upload', upload.single('test'), (req, res) => { 
 	const input = req.body
-	console.log(req.file);
-	//file.location이 이미지 저장된 링크
-	connection.query(`insert into pill_image_url values(${ input.id }, '${ req.file.location }')`), (err, rows, fields) => {
+	// console.log(req.file);
+	//db에 사용자 id와 이미지 저장된 링크 저장
+	connection.query(`insert into pill_image_url values(${input.id}, '${req.file.location}')`, (err, rows) => {
 		if (err) {
 			return res.json({ success: false, err });
 		}
-		res.send(rows);
-	}
+		return res.status(200).json({ //200은 성공했다는 뜻
+			success: true
+		})
+		// console.log(rows)
+		// res.send(rows);
+	});
 });
 
 app.post('/search', (req, res) => {
 	const input = req.body
-	res.send(input)
+	//식별문자 앞 뒤가 있네,, 뒷편 문자는 optional한디 그러면 어쨌든 빈값 들어오는거 고려해야
+	//분할선도 앞 뒤가 있네,,,
+	//근데 이렇게 쓰면 유저가 앞뒤를 어떻게 구별하지?
+	var sql = 'select name from pills where (char_front=? or char_back=?) \
+				and (line_front=? or line_back=?) and shape=? pill_type=? and color=?'
+	var params = [input.char_front, input.char_front, input.line_front, 
+				input.line_back, input.shape, input.pill_type, input.color]
+	if (input.char_back) {
+		sql += 'and char_back=?'
+		params.push(input.char_back)
+	}
+	//sql문 줄여야할 것 같은데 escape vs ?
+	// 클라이언트로 식별번호나 약 이름 전달
+	connection.query(sql, params, (err, result) => {
+	// connection.query('select name from pills where char_front=? and line_front=? and shape=? pill_type=? and color=?', [input.shape, input.color], (err, result, fields) => {
+	// connection.query('select name from pills where shape=?', [input.shape], (err, result, fields) => {
+	if(err) return res.json({ success: false, err })
+	//만약에 찾는 약이 없으면?
+	//검색결과가 있다면
+	if (result.length > 0) {
+		res.redirect('/check?searchData=' + encodeURIComponent(JSON.stringify(result)));
+	} else {
+		// 검색 결과가 없으면
+		res.redirect('/search-fail');
+	}
 })
 
 app.get('/search-fail', (req, res) => {
@@ -51,19 +81,14 @@ app.get('/loading', (req, res) => {
 })
 
 app.post('/check', (req, res) => {
-	const input = req.body
-	//식별문자 앞 뒤가 있네,, 뒷편 문자는 optional한디 그러면 어쨌든 빈값 들어오는거 고려해야
-	//분할선도 앞 뒤가 있네,,,
-	//sql문 줄여야할 것 같은데 escape vs ?
-	connection.query('select * from pills where char_front=? and line_front=? and shape=? pill_type=? and color=?', [input.shape, input.color], (err, rows, fields) => {
-		if(err) return res.json({ success: false, err })
-		res.send(row.name)
-	// 클라이언트로 식별번호나 약 이름 전달
+	// /search에서 보낸 결과
+	const searchData = JSON.parse(req.query.searchData);
+	res.send('약 맞는지 확인')
 	})
 })
 
 app.get('/info', (req, res) => {
-	res.send('검색 중')
+	res.send('약 정보 출력')
 })
 
 app.use((req, res, next) => {
@@ -76,7 +101,7 @@ app.use((req, res, next) => {
 	res.locals.message = err.message;
 	res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
 	res.status(err.status || 500);
-	res.render('error');
+	res.json({ index_success: false, err });
   });
 
 // router.post('/image', upload.single('image'), controller.image.post);
--- modules/multer.js
+++ modules/multer.js
@@ -4,7 +4,7 @@ const multerS3 = require('multer-s3');
 
 // 파일 이름을 위한 uuid 생성
 const { v4 } = require('uuid');
-console.log(v4());
+// console.log(v4());
 const uuid = v4();
 
 // 이미지 저장할 곳
@@ -14,7 +14,7 @@ const upload = multer({
 		s3: s3,
 		bucket: 'user-pills',
 		key: function (req, file, cb) {
-		cb(null, `${ Date.now() }${ uuid }`) 
+		cb(null, `${Date.now()}${uuid}`) 
 		},
 	}),
 	limit: { filesize: 5 * 1024 * 1024 } //파일 크기 5MB 이하
